8ef11b98badf4f00c456934a42772fbdade7c3bb
Fahima <efahima76@gmail.com>
2021-10-03 14:17:10 -0400

    update Prog1

--- src/Programs/Prog1.java
+++ src/Programs/Prog1.java
@@ -3,5 +3,7 @@ package Programs;
 public class Prog1 {
     public static void main(String[] args) {
         System.out.println("Enter item numbers");
+        String item1;
+        System.out.println("Enter your first item");
     }
 }
