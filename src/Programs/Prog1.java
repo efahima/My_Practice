a45323a8c3d8151361c8df3292da77c08520d1bd
Fahima <efahima76@gmail.com>
2021-10-03 14:18:29 -0400

    Second  update on Prog1 to print

--- src/Programs/Prog1.java
+++ src/Programs/Prog1.java
@@ -4,6 +4,6 @@ public class Prog1 {
     public static void main(String[] args) {
         System.out.println("Enter item numbers");
         String item1="Pen";
-        System.out.println("Enter your first item");
+        System.out.println(item1);
     }
 }
